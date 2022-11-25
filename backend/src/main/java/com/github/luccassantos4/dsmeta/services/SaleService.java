807f48c656e4d3a030760091e019720cea0dc9fb
luccassantos4 <luccassantos4@outlook.com>
2022-11-25 16:57:15 -0300

    Twilio SMS

--- backend/src/main/java/com/github/luccassantos4/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/github/luccassantos4/dsmeta/services/SaleService.java
@@ -18,13 +18,12 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	
 	public Page<Sale>findSales(Pageable pageable, String minDate, String maxDate) {
 		
 		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
 		
 		
-		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate min = minDate.equals("") ? today.minusDays(365)  : LocalDate.parse(minDate);
 		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
 		
 		return repository.findSales(min, max, pageable);
