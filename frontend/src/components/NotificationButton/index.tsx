225dc8cd30a0a74f80799907c5ce6095731bb10a
luccassantos4 <luccassantos4@outlook.com>
2022-11-28 17:02:40 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+   saleId: number;
+}
+
+function handleCLick(id : number){
+  axios(`${BASE_URL}/sales/${id}/notification`).then(Response => {
+    console.log("deu bom")
+  })
+}
+
+
+function NotificationButton( {saleId} : Props) {
     return(
-      <div className="dsmeta-red-btn">
+      <div className="dsmeta-red-btn" onClick={() => handleCLick(saleId)}>
         <img src={icon} alt="Notificar" />
       </div>
     )
